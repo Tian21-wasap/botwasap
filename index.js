6dce60c10f461d9d4e1a56e6beb37523b69036a6
Tian21-wasap <75293727+Tian21-wasap@users.noreply.github.com>
2021-01-21 18:06:46 +0700

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const  XBOT = 'AMPIBI'; // Nama Bot Whatsapp
-const instagram = 'https://instagram.com/affis_saputro123'; // Nama Instagramlu cok
-const nomer = 'https://Wa.me/+6282334297175'; // Nomor whatsapplu cok
+const  XBOT = 'FIJAR'; // Nama Bot Whatsapp
+const instagram = 'KAGAK ADA'; // Nama Instagramlu cok
+const nomer = '082320852114'; // Nomor whatsapplu cok
 const aktif = 'Tergantung kuota'; // Kapan bot lu aktif
 const groupwa = 'comming soon'; // OFFICIAL GRUP LU 1
-const youtube = 'https://youtube.com/channel/UCYKxsg7iF9a9IZyXQRNsvqw'; 
+const youtube = 'GAK ADA'; 
 const qrcode = require("qrcode-terminal");
 const moment = require("moment");
 const cheerio = require("cheerio");
